fc243b7f5af09c91cd506a3cc4cf505ac6fcaeaa
MilesXu <kanonxmm@163.com>
2024-01-09 10:42:05 +0800

    for mac

--- infer_app.py
+++ infer_app.py
@@ -8,9 +8,9 @@ os.environ['CUDA_VISIBLE_DEVICES'] = '0'
 from ultralytics import YOLO
 
 def infer():
-    model = YOLO('.\\8mpt\\best.pt')
-    results = model('.\\imgs\\train_6676.jpg')
-    print(results[0].plot())
+    model = YOLO('8npt/best.pt')
+    results = model('/Users/milesxu/Projects/Money/gbcrawler/imgs/train_6577.jpg')
+
     cv2.imwrite('./result.png', results[0].plot())
 
 if __name__ == '__main__':
